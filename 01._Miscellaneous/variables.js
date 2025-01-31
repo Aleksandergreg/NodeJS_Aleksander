30e458a6e5285215f151668e6f047078f2e20dc7
Aleksandergreg <alek3691@stud.kea.dk>
2025-01-31 09:54:37 +0100

    Test

---
+++ 01._Miscellaneous/variables.js
@@ -0,0 +1,15 @@
+//avoid var
+//const name = 'Aleksander';
+
+// const is NOT a constant
+// that means that const CAN have it's value changed
+// const is a constant in the DECLARATION
+// const MUST be decalred as it is initialized
+
+const me = {};
+
+me.name = "Anders";
+
+//name = 'Thea'
+
+// "" vs. ' 

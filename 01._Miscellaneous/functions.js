5eb83aaa3d26452b608c0ececb33107556ce122a
Aleksandergreg <alek3691@stud.kea.dk>
2025-02-07 09:35:54 +0100

    more function exercises

--- 01._Miscellaneous/functions.js
+++ 01._Miscellaneous/functions.js
@@ -6,3 +6,50 @@ console.log(getRandomInt(0,10));
 function getRandomInt(min, max){
   return Math.floor(Math.random() * (max + 1 - min) - min);
 }
+
+const getRandomIntAnonymousFunction = function (min, max){
+    return Math.floor(Math.random() * (max + 1 - min) - min);
+  };
+
+  const getRandomIntArrowFunction = (min, max) => {
+    return Math.floor(Math.random() * (max +1 -min) - min);
+  };
+
+  console.log(getRandomIntArrowFunction(0,10));
+
+                //name:string   action: function
+                //action is a callback function
+//a callback is a function that is passed as a paramterr to another function
+//possibly with the aim of calling it later (but not necessarily)
+  function genericPerformer(name, action) {
+    return action(name);
+  }
+//sende en funktion der muligvis kan blive kaldt senere: callback
+//Lasse coding
+const codingAction = (name) => `${name} likes coding.`;
+
+//assignment: using the generic performer, I would like it to say:
+//assignment Lasse likes coding.. write it below
+
+console.log(genericPerformer("Lasse", codingAction));
+
+//Prints [Function: getRandomInt]: a function reference
+console.log(getRandomInt);
+
+//Andreas sleeping
+
+function sleepingAction(name){
+    return `${name} loves sleeping;`
+}
+
+// Assignment: Andreas loves sleeping
+
+console.log(genericPerformer("Andreas", sleepingAction));
+
+//tara boxing
+//assignment: the desired result is: tara owns at boxing
+//write it all as a one-liner
+//const codingAction = (name) => `${name} likes coding.`;
+
+console.log(genericPerformer("Tara", (name) => `${name} likes boxing`))
+

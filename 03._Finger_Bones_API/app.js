b98491fb10a7924fd26f5d66a0bd06a8b60826d5
Aleksandergreg <alek3691@stud.kea.dk>
2025-02-13 15:29:28 +0100

    Finished the Rest API - Part I exercise

--- 03._Finger_Bones_API/app.js
+++ 03._Finger_Bones_API/app.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const app = express();
 
+const fingerbones = [
+    { id: 1, name: "Index", details: "..." },
+    { id: 2, name: "Middle", details: "..." }
+]
+
+app.get("/fingerbones", (req, res) => {
+    res.json({ data: fingerbones });
+});
+
+app.get("/fingerbones/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const fingerbone = fingerbones.find(fb => fb.id === id);
+
+    if (!fingerbone) {
+        return res.status(404).json({ error: "Fingerbone not found" });
+    }
+
+    res.json({ data: fingerbone });
+});
+
+
+app.listen(8080);

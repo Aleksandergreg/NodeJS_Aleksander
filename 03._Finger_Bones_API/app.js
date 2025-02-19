3b0a675d59dab01551ff4c52f96fdb7e3ec8e4d8
Aleksandergreg <alek3691@stud.kea.dk>
2025-02-19 11:32:46 +0100

    Added post method for adding a fingerbone

--- 03._Finger_Bones_API/app.js
+++ 03._Finger_Bones_API/app.js
@@ -5,12 +5,16 @@ const express = require('express');
 //instansiere express
 const app = express();
 
+//Middleware to parse json bodies. ASK ANDERS ABOUT THIS
+app.use(express.json());
+
 const fingerBonesRouter = express.Router();
 
 const fingerBones = [
     { id: 1, name: "Index", details: "..." },
     { id: 2, name: "Middle", details: "..." }
 ]
+let nextId = 3;
 
 fingerBonesRouter.get("/", (req, res) => {
     res.send({ data: fingerBones });
@@ -21,10 +25,27 @@ fingerBonesRouter.get("/:id", (req, res) => {
     const foundFingerBone = fingerBones.find(fb => fb.id === id);
 
     if (!foundFingerBone) {
-        return res.status(404).json({ error: "Fingerbone not found" });
+        return res.status(404).send({ error: "Fingerbone not found" });
     }
 
     res.send({ data: foundFingerBone });
+});
+
+fingerBonesRouter.post("/", (req, res) => {
+    const { name, details } = req.body;
+
+    if(!name) {
+        return res.status(400).send({ error: "Name is required" });
+    }
+
+    const newFingerBone = {
+        id: nextId ++,
+        name,
+        details: details || ""
+    };
+    
+    fingerBones.push(newFingerBone);
+    res.status(201).send({ data: newFingerBone });
 });
 
 app.use('/fingerbones', fingerBonesRouter)

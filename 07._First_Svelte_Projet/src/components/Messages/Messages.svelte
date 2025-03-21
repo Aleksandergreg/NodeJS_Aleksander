dff892ef51a136c8a0958d0b536c4ba670d7f3d2
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-21 11:12:07 +0100

    Finished First Svelte Project

---
+++ 07._First_Svelte_Projet/src/components/Messages/Messages.svelte
@@ -0,0 +1,20 @@
+<script>
+    const messages = $state(["Hello", "Mother"]);
+
+    let messageInput = $state("");
+
+
+    function saveMessage() {
+        messages.push(messageInput);
+        messageInput="";
+        // messages=[...messages, messageInput]
+        console.log(messages);
+    }
+</script>
+
+<h1>Messages</h1>
+
+<input bind:value={messageInput} placeholder="Type your message..">
+<p>{messages}</p>
+
+<button onclick={saveMessage}>Submit Message</button>
